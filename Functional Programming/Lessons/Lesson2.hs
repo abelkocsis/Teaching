009a24af66c3fa2155b9b598181b3630e606e62a
abelkocsis <kocsis.abel.98@gmail.com>
2019-09-11 18:47:41 +0200

    added Lesson2

---
+++ Functional Programming/Lessons/Lesson2.hs
@@ -0,0 +1,11 @@
+-- Feladat: Másodpercek száma
+mp = 60 * 60 * 24 * 365
+
+-- Feladat: Gömb térfogata
+v = 4 * 1.01 ^ 3 * pi / 3
+
+-- Feladat: Oszthatóság
+oszthato = 532253373 `mod` 23
+
+-- Feladat: Zárójelezés
+mego = 1 + ((2 * (3 ** 4)) / 5)
