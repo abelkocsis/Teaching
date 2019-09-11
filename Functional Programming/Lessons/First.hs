02267ff66a1c011beb9c9cca40d7852ce057c6ea
abelkocsis <kocsis.abel.98@gmail.com>
2019-09-11 16:04:19 +0200

    added first lesson

---
+++ Functional Programming/Lessons/First.hs
@@ -0,0 +1,35 @@
+x = 12
+x = 24
+
+--4
+myFirstVariable= "Hello world!"
+myFirstFunction a = a + 12
+
+number = 12
+boolean = True
+character = 'a'
+string1 = "String"
+
+--6
+inc a = a + 1
+
+--7
+even' a = mod a 2 == 0
+
+--8
+odd' a = mod a 2 == 1
+
+--9
+odd'' a = not (even' a)
+
+--10
+divides a b = mod a b == 0
+
+--11
+area a b = a * b 
+
+--12
+triangleSides a b c = a + b > c && a + c > b && b + c > a
+
+--13
+pythagoreanTriple a b c = a * a + b * b == c * c
