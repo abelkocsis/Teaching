2ce019f3fd3dad76303891c7b763043f85c9fcc5
abelkocsis <kocsis.abel.98@gmail.com>
2019-11-29 14:56:49 +0100

    Lesson11.hs add

---
+++ Functional Programming/Lessons/Lesson11.hs
@@ -0,0 +1,67 @@
+--https://people.inf.elte.hu/poor_a/fp12.pdf
+--1
+data Day = Hetfo | Kedd | Szerda | Csutortok | Pentek | Szombat | Vasarnap
+
+today :: Day -> Day
+today d = d
+
+--2
+isFirstDayOfWeek :: Day -> Bool
+isFirstDayOfWeek Hetfo = True
+isFirstDayOfWeek _ = False
+
+--3
+isWeekend :: Day -> Bool
+isWeekend Szombat = True
+isWeekend Vasarnap = True
+
+--típusszinonima:
+type Hours = Integer
+type Minutes = Integer
+
+--4
+data Time = T Hours Minutes
+
+--5
+showTime :: Time -> [Char]
+showTime (T a b) = show a ++ "." ++ show b
+
+--6
+eqTime :: Time -> Time -> Bool
+eqTime (T h1 m1) (T h2 m2) = h1 == h2 && m1 == m2
+
+--7
+isEarlier :: Time -> Time -> Bool
+isEarlier (T h1 m1) (T h2 m2) = h1 < h2 || h1 == h2 && m1 < m2
+
+--8
+isBetween t1 t2 t3 = isEarlier t1 t2 && isEarlier t2 t3 || isEarlier t3 t2 && isEarlier t2 t1
+
+--9
+time :: Hours -> Minutes -> Time
+time h m 
+    | h < 0 || h > 23 || m < 0 || m > 59 = error "Invalid time"
+    | otherwise = T h m 
+
+--10
+data USTime = AM Hours Minutes | PM Hours Minutes
+    deriving (Eq, Show)
+
+--11
+showUSTime :: USTime -> [Char]
+showUSTime (AM h m) = show h ++ "." ++ show m ++ " am"
+showUSTime (PM h m) = show h ++ "." ++ show m ++ " pm"
+
+--12
+usTimeToTime (AM 12 m) = T 0 m
+usTimeToTime (AM h m) = T h m
+usTimeToTime (PM 12 m) = T 12 m
+usTimeToTime (PM h m) = T (h+12) m
+
+--13
+timeToUSTime (T h m)
+    | h == 0 = AM 12 m    
+    | h < 12 = AM h m
+    | h == 12 = PM 12 m
+    | otherwise = PM (h-12) m
+
